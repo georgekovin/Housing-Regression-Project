db23a060dc8671600b454ac1a88639155480ec88
georgekovin <108282258+georgekovin@users.noreply.github.com>
2023-10-19 19:45:09 +0300

    commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+"""Библиотека с техническими функциями, 
+    которые используются в ноутбуках."""
+
+import pandas as pd
+
+
+def get_data_info(data):
+    data_info = pd.DataFrame({'Nulls': (data.isna().sum() * 100/data.shape[0]).round(2), 
+                            'Uniques': data.nunique(dropna=False),
+                            'Dtypes': data.dtypes})
+
+    return data_info
+
+
+get_populars = lambda data, first=100: (data
+                                        .value_counts(dropna=False)[:first]
+                                        .index
+                                        .to_list())
+
+
+get_sums = lambda data, name: data.loc[:, list(filter(lambda x: name in x, data.columns))].sum()
+
+
