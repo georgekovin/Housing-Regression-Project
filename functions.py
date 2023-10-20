baee69c74bde0e264cc63696db9f3a4db79ed9b2
georgekovin <108282258+georgekovin@users.noreply.github.com>
2023-10-20 16:02:40 +0300

    cummit

--- functions.py
+++ functions.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 
 def get_data_info(data):
+    """Выводит на экран основную информацию о датасете: 
+    количество пропусков в процентном соотношении, 
+    количество уникальных значений и типы данных каждого признака."""
+    
     data_info = pd.DataFrame({'Nulls': (data.isna().sum() * 100/data.shape[0]).round(2), 
                             'Uniques': data.nunique(dropna=False),
                             'Dtypes': data.dtypes})
@@ -16,9 +20,12 @@ get_populars = lambda data, first=100: (data
                                         .value_counts(dropna=False)[:first]
                                         .index
                                         .to_list())
+"""Выводит на экран (по умолчанию) 100 самых часто встречающихся значений в признаке. """
 
 
 get_sums = lambda data, name: data.loc[:, list(filter(lambda x: name in x, data.columns))].sum()
+"""Нужна для бинарных переменных, считает суммы по каждому столбцу, где в названии встречается какое-то слово."""
 
 
 get_vc = lambda data, first=20: data.value_counts().iloc[:first]
+"""Выводит на экран (по умолчанию) 20 самых популярных значений в столбце и количество их в нем."""
