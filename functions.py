5d61d40c824d4df33c48528b956406dcda634770
georgekovin <108282258+georgekovin@users.noreply.github.com>
2023-10-29 23:15:27 +0300

    docstrings completed

--- functions.py
+++ functions.py
@@ -10,8 +10,8 @@ def get_data_info(data):
     количество уникальных значений и типы данных каждого признака."""
     
     data_info = pd.DataFrame({'Nulls': (data.isna().sum() * 100/data.shape[0]).round(2), 
-                            'Uniques': data.nunique(dropna=False),
-                            'Dtypes': data.dtypes})
+                              'Uniques': data.nunique(dropna=False),
+                              'Dtypes': data.dtypes})
 
     return data_info
 
@@ -20,12 +20,21 @@ get_populars = lambda data, first=100: (data
                                         .value_counts(dropna=False)[:first]
                                         .index
                                         .to_list())
-"""Выводит на экран (по умолчанию) 100 самых часто встречающихся значений в признаке. """
+"""Выводит на экран (по умолчанию) 100 самых часто встречающихся значений в признаке.\n
+    - data: сами данные
+    - first: сколько первых значений нужно выделить
+"""
 
 
 get_sums = lambda data, name: data.loc[:, list(filter(lambda x: name in x, data.columns))].sum()
-"""Нужна для бинарных переменных, считает суммы по каждому столбцу, где в названии встречается какое-то слово."""
+"""Нужна для бинарных переменных, считает суммы по каждому столбцу, где в названии встречается какое-то слово.
+    - data: сами данные
+    - name: слово, которое встречается в названиях столбцов
+"""
 
 
 get_vc = lambda data, first=20: data.value_counts(ascending=False).iloc[:first]
-"""Выводит на экран (по умолчанию) 20 самых популярных значений в столбце и количество их в нем."""
+"""Выводит на экран (по умолчанию) 20 самых популярных значений в столбце и количество их в нем.
+    - data: сами данные
+    - first: сколько первых значений нужно выделить
+""" 
